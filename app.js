782427bea7670a3003076516c55be16118498947
AlanAcosta098 <95245525+AlanAcosta098@users.noreply.github.com>
2022-11-29 16:32:42 -0600

    Tarea 6

---
+++ app.js
@@ -0,0 +1,24 @@
+function despedir() {
+	alert("Adios desde un archivo externo");
+	
+}
+
+function Hola() {
+	alert("Hola xdddddd");
+
+}
+function Adios() {
+	alert("Adios xddddd");
+
+}
+function HastaLuego() {
+	alert("Adios desde un archivo externo");
+
+}
+console.log("Hola desde la consola");
+console.log(20);
+console.log(2*6);
+console.log(1,63);
+console.log(1+2);
+console.log(true);
+console.log(false);
